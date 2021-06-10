e29bc72dd5a3ffe1043bec6217bf1cd1ae6491fc
monkeyz <sangmin3285@gmail.com>
2021-06-10 13:51:55 +0900

    modified 0609 add 0610

--- 20210609-2.py
+++ 20210609-2.py
@@ -5,9 +5,22 @@ import sys
 def recursive_funtion(store_model, customer_Num, customer_model):
     store_model.sort()
     customer_model.sort()
+    start = store_model[0]
+    end = store_model[-1]
+    mid  = (start + end) // 2
+    for i in range(int(customer_Num)):
+        while start <= end:
+            if customer_model[i] > end:
+                return 'no'
+            elif mid > customer_model[i]:
+                # 지금 5보다 낮다는 거야
+                
 
-    print(store_model[-1])
-    print(store_model)
+
+
+            elif mid <= customer_model[i]:
+                return 'no'
+
     # 그러니까 3번은 결과를 봐야하잖아 그럼 해보자
     # for i in range(int(customer_Num)):
     #     if 
