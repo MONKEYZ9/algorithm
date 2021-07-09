742f8f3742f1602cf8e42859b5cf7cd82fd75e0a
monkeyz <sangmin3285@gmail.com>
2021-07-09 18:16:47 +0900

    add sql_practice

--- 20210708.py
+++ 20210708.py
@@ -1,3 +1,10 @@
+# len(list)
+# 어떻게 하면 len을 안스고 할 수 있는가
+# 리스트의 시간복잡도를 구하는 시간은 뭘까
+# 푸쉬와 팝만 할 수 있다고    하면 괜찮다는 거야
+
+
+
 def run_cmd_with_stack(cmd, st):
     cmd_type = cmd[0]
 
@@ -5,6 +12,7 @@ def run_cmd_with_stack(cmd, st):
         _, num = cmd #이렇게 하면 cmd[1]안해도 되고 이렇게 하면 된다는 거야
         st.append(num)
     elif cmd_type == 'pop':
+
         print(stack.pop()) # 이렇게 짯을거다
     elif command[0] == 'size':
         print( len(st))
