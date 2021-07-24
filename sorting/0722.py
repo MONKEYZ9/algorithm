ce53b3696cb0f8739ca47e90279a676c43e0e73e
monkeyz <sangmin3285@gmail.com>
2021-07-25 01:06:54 +0900

    update 0722

--- sorting/0722.py
+++ sorting/0722.py
@@ -33,6 +33,28 @@ def selection_sort(num_list, n):
         del dict[remove_key[-1]]
     return new_num_list
 
+def bubble_sort(num_list, n):
+    dict = {i : num for i, num in enumerate(num_list)}
+    # 밑의 과정을 반복할거니까 반복문을 써보자
+    for i in range(n-1, 0, -1):
+        if i == 0:
+            print('끝', dict.values(), type(dict.values()))
+            return dict.values()
+        start_num = 0
+        print('i : ', i)
+        print('++++++++++++++++++++++++++')
+        while start_num != i:
+            print('start_num : ', start_num)
+            if dict[start_num] > dict[start_num + 1]:
+                print(dict[start_num], dict[start_num + 1])
+                dict[start_num], dict[start_num + 1] = dict[start_num + 1], dict[start_num]
+                print(start_num)
+                print(dict[start_num], dict[start_num + 1])
+                print('끝', dict.values(), type(dict.values()))
+                print('======================')
+            start_num += 1
+        
+
 n = int(input())
 num_list = []
 
@@ -42,8 +64,8 @@ for _ in range(n):
 
 # insertion_sorted_list = insertion_sort(num_list, n)
 # print(" ".join(map(str, insertion_sorted_list)))
-selection_sorted_list = selection_sort(num_list, n)
-print(" ".join(map(str, selection_sorted_list)))
+# selection_sorted_list = selection_sort(num_list, n)
+# print(" ".join(map(str, selection_sorted_list)))
 # ​
-# bubble_sorted_list = bubble_sort()
-# print(" ".join(map(str, bubble_sorted_list)))
+bubble_sorted_list = bubble_sort(num_list, n)
+print(" ".join(map(str, bubble_sorted_list)))
