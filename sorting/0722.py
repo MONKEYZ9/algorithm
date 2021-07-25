999e2b3d1b9e49187f1b9979bacc0c30ee870d80
monkeyz <sangmin3285@gmail.com>
2021-07-26 08:40:18 +0900

    update 0722

--- sorting/0722.py
+++ sorting/0722.py
@@ -37,22 +37,12 @@ def bubble_sort(num_list, n):
     dict = {i : num for i, num in enumerate(num_list)}
     # 밑의 과정을 반복할거니까 반복문을 써보자
     for i in range(n-1, 0, -1):
-        if i == 0:
-            print('끝', dict.values(), type(dict.values()))
-            return dict.values()
         start_num = 0
-        print('i : ', i)
-        print('++++++++++++++++++++++++++')
         while start_num != i:
-            print('start_num : ', start_num)
             if dict[start_num] > dict[start_num + 1]:
-                print(dict[start_num], dict[start_num + 1])
                 dict[start_num], dict[start_num + 1] = dict[start_num + 1], dict[start_num]
-                print(start_num)
-                print(dict[start_num], dict[start_num + 1])
-                print('끝', dict.values(), type(dict.values()))
-                print('======================')
             start_num += 1
+    return dict.values()
         
 
 n = int(input())
@@ -62,10 +52,9 @@ for _ in range(n):
     num = int(input())
     num_list.append(num)
 
-# insertion_sorted_list = insertion_sort(num_list, n)
-# print(" ".join(map(str, insertion_sorted_list)))
-# selection_sorted_list = selection_sort(num_list, n)
-# print(" ".join(map(str, selection_sorted_list)))
-# ​
+insertion_sorted_list = insertion_sort(num_list, n)
+print(" ".join(map(str, insertion_sorted_list)))
+selection_sorted_list = selection_sort(num_list, n)
+print(" ".join(map(str, selection_sorted_list)))
 bubble_sorted_list = bubble_sort(num_list, n)
 print(" ".join(map(str, bubble_sorted_list)))
