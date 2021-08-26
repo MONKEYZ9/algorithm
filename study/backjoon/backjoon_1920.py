1e397bcf3722b68e2683db3b4dba972d3d019e41
monkeyz <sangmin3285@gmail.com>
2021-08-26 15:14:12 +0900

    add 0826-수찾기 완성

--- study/backjoon/backjoon_1920.py
+++ study/backjoon/backjoon_1920.py
@@ -10,24 +10,49 @@ N_list.sort()
 # 있으면 1 없으면 0을 내 뱉는
 
 
-def check_num(N, N_list):
-  mid_num = N_list[len(N_list)//2]
-  last_num = N_list[-1]
-  fisrt_num = N_list[0]
-  if N == mid_num:
+# 1 2 3 4 5
+# 1 3 5 7 9
+
+def check_num(start_num, end_num, M, N_list):
+  if start_num > end_num:
+    return 0
+
+  mid_num = (start_num+end_num)//2
+
+  if M == N_list[mid_num]:
     return 1
-  elif N > mid_num:
-    if N > last_num:
-      return 0
-    N_list = 
-    return check_num(N, N_list[(len(N_list)//2)+1:])
-  elif N < mid_num:
-    if N < fisrt_num:
-      return 0
-    return check_num(N, N_list[:(len(N_list)//2)])
 
+  elif M < N_list[mid_num]:
+    return check_num(start_num, mid_num-1, M, N_list)
+
+  elif M > N_list[mid_num]:
+    return check_num(mid_num+1, end_num, M, N_list)
+  
 for i in M_list:
-  ans = check_num(i, N_list)
-  print(ans)
+  print(check_num(0, len(N_list)-1, i, N_list))
 
+# def check_num(start_num, end_num, M, N_list):
+  
+#   while True:
+#     mid_num = (start_num+end_num)//2
+#     mid_N = N_list[(start_num+end_num)//2]
+#     if M == mid_N:
+#       return 1
+#     elif M < mid_N:
+#       end_num = mid_num
 
+#     elif M > mid_N:
+#       start_num = mid_num
+  
+# for M in M_list:
+#   start_num = 0
+#   end_num = N-1
+#   if M > N_list[end_num] or M < N_list[start_num]:
+#     print(0)
+
+#   elif M == N_list[end_num] or M == N_list[start_num]:
+#     print(1)
+
+#   else:
+#     ans = check_num(start_num, end_num+1, M, N_list)
+#     print(ans)
